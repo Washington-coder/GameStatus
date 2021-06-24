c601b2ec4c9ba171ffa2e4c45cc7985539d38995
Washington-coder <was.antonio.xy@gmail.com>
2021-06-23 22:16:25 -0400

    initial image

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, Image } from "react-native";
 
+import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
   const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Hello sign in</Text>
-      <TextInput style={styles.input} onChangeText={setText}></TextInput>
-      <Text>Voce digitou {text}</Text>
+      <Image source={IllustrationImg} style={styles.image}></Image>
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {`\n`}
+          suas jogatinas {`\n`}
+          facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {`\n`}
+          favoritos com seus amigos
+        </Text>
+      </View>
     </View>
   );
 }
