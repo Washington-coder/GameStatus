e0236bbb32a9d77c862d1d29fbd56af7e873bd27
Washington-coder <was.antonio.xy@gmail.com>
2021-06-23 21:35:24 -0400

    images update

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput } from "react-native";
 
 import { styles } from "./styles";
 
 export function SignIn() {
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
       <Text>Hello sign in</Text>
+      <TextInput style={styles.input} onChangeText={setText}></TextInput>
+      <Text>Voce digitou {text}</Text>
     </View>
   );
 }
