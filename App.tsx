c3c7570ac4ba28ac9870a2119ab7cb26b8dc90be
Washington-coder <was.antonio.xy@gmail.com>
2021-06-24 16:38:53 -0400

    Linear gradient update

--- App.tsx
+++ App.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
 import AppLoading from "expo-app-loading";
+import { SignIn } from "./src/screens/Signin/index";
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
 import {
   Rajdhani_500Medium,
   Rajdhani_700Bold,
 } from "@expo-google-fonts/rajdhani";
-
-import { SignIn } from "./src/screens/Signin/index";
-import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,13 +23,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar
         barStyle={"light-content"}
         backgroundColor="transparent"
         translucent
       ></StatusBar>
-      <SignIn />{" "}
-    </>
+      <SignIn />
+    </Background>
   );
 }
