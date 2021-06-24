588d042b9cf891daf78e4cd8989ed4a312397d8c
Washington-coder <was.antonio.xy@gmail.com>
2021-06-23 22:45:52 -0400

    Screen 1 styles

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -5,16 +5,21 @@ import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
-  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Image source={IllustrationImg} style={styles.image}></Image>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      ></Image>
+
       <View style={styles.content}>
         <Text style={styles.title}>
           Organize {`\n`}
           suas jogatinas {`\n`}
           facilmente
         </Text>
+
         <Text style={styles.subtitle}>
           Crie grupos para jogar seus games {`\n`}
           favoritos com seus amigos
