52763d51ef48b20b32b2c0326cde2e214c80e740
Washington-coder <was.antonio.xy@gmail.com>
2021-06-24 15:25:27 -0400

    Fonts update

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useFonts } from "expo-font";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+import AppLoading from "expo-app-loading";
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
 
 import { SignIn } from "./src/screens/Signin/index";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_700Bold,
+    Rajdhani_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <SignIn />;
 }
--- src/screens/Signin/styles.ts
+++ src/screens/Signin/styles.ts
@@ -6,7 +6,7 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: theme.colors.background,
+    backgroundColor: theme.colors.secondary100,
   },
   image: {
     width: "100%",
@@ -22,11 +22,13 @@ export const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 30,
     marginBottom: 16,
+    fontFamily: theme.fonts.title700,
   },
   subtitle: {
     color: theme.colors.heading,
     fontSize: 15,
     textAlign: "center",
     marginBottom: 34,
+    fontFamily: theme.fonts.title500,
   },
 });
