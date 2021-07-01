7b95725b8d75953b225e781f95662971e9c94fcd
Washington-coder <was.antonio.xy@gmail.com>
2021-07-01 09:26:30 -0400

    Edit - ButtonIcon

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,27 +1,21 @@
 import React from "react";
-import {
-  Text,
-  Image,
-  View,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { Text, Image, View } from "react-native";
 
 import DiscordImg from "../../assets/discord.png";
 import { styles } from "./styles";
-import { useNavigation } from "@react-navigation/native";
 
-type Props = TouchableOpacityProps & {
+type Props = RectButtonProps & {
   title: string;
 };
 
 export function ButtonIcon({ title, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.5} {...rest}>
+    <RectButton style={styles.container} activeOpacity={0.5} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordImg} style={styles.icon}></Image>
       </View>
       <Text style={styles.title}>{title}</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
